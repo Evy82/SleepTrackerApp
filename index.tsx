b464eec4bb34d0bf15c96820fd80a770f28b79d2
PatriciaDev <109634022+Evy82@users.noreply.github.com>
2024-07-29 14:16:49 +0200

    Init

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import dotenv from 'dotenv';
+dotenv.config();
+ReactDOM.render(
+  <React.StrictMode>
+    <Preformatted text>App /></React.StrictMode>,
+  document.getElementById('root')
+);
